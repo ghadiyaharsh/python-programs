46843ef2f8e8f7c243f48b11b3980e626eebcd18
ghadiyaharsh <harshghadiya212@gmail.com>
2024-12-17 23:17:33 +0530

    Added python program for loop

---
+++ 5loop.py
@@ -0,0 +1,18 @@
+#
+str="amit'shouse"
+for char in str:
+            
+ print(char)
+
+
+
+
+#range  :-   range(start?,stop,step?)
+
+ seq = range(5)    #range(stop)
+ for i in seq:     #range(start,stop)
+  print(i)              
+
+
+for i in range(2,100,2):
+ print(i)           
