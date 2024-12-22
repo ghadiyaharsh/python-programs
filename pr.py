49c1003264176a658d9bfbabeabce2bc86a95084
ghadiyaharsh <harshghadiya212@gmail.com>
2024-12-22 11:36:33 +0530

    Remove files from GitHub but keep locally

--- pr.py
+++
@@ -1,8 +0,0 @@
-#helloworld.py practice
-
-#  1) write a program to input 2 numbers & print their sum
-
-num1 = int(input("enter the number 1 :-"))
-num2 = int(input("enter the number of 2:-"))
-
-print("the sum of number1  and number2 is :-",num1+num2)
