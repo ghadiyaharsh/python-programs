c3be4f932f8377830448b3f24431d2a2e2aca626
Harsh Ghadiya <harshghadiya212@gmail.com>
2024-12-23 21:20:28 +0530

    Update eliminate_duplicate_List.py

--- eliminate_duplicate_List.py
+++ eliminate_duplicate_List.py
@@ -5,11 +5,11 @@ def remove_duplicates(input_list):
     return list(set(input_list))
 
 example_list = [1, 2, 2, 3, 4, 4, 5, 6, 6, 6]    # Example list with duplicate values
-new_list = remove_duplicates(example_list)   # Removing duplicates
+new_list = remove_duplicates(example_list)   # Removing duplicates   
 
 
 print("Original List:", example_list)
-print("List with eliminates  Duplicates:", new_list)
+print("List with eliminates  Duplicates:", new_list)      #[1,2,3,4,5,6]
 
 
 '''set(): Converts the list to a set, which automatically removes duplicate values because sets do not allow duplicates.
