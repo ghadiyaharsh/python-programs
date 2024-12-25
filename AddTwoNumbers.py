5a15f99b1f82d11be762a2a212cb24869a7d5485
ghadiyaharsh <harshghadiya212@gmail.com>
2024-12-25 20:01:52 +0530

    added summation of two numbers

---
+++ AddTwoNumbers.py
@@ -0,0 +1,11 @@
+#predefined numbers are taken and added
+num1=56
+num2=98
+print("Addition of num1 and num2 is:", num1+num2)
+
+#number is taken from user and added
+num1=int(input("enter the number1:"))
+num2=int(input("enter the number2:"))
+print(num1+num2)
+
+#user give any number like decimal,floting point,integer etc...
