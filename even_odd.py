ebd6af3493b86a98f0ac7f10280ce98beffdb112
ghadiyaharsh <harshghadiya212@gmail.com>
2025-01-02 13:05:09 +0530

    even odd program

---
+++ even_odd.py
@@ -0,0 +1,12 @@
+#write a python program to implement the given number is primr or not
+number1=int(input("enter the number :-"))
+if number1/2:
+     print(number1, "is even number")
+elif number1<0: 
+     print(number1," is negetive number ")  
+else :
+    print(number1," is odd number")       
+    
+    
+    
+            
