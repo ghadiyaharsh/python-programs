1a370014728da491e5735d89951bfca37a5b17d0
ghadiyaharsh <harshghadiya212@gmail.com>
2025-02-10 11:37:46 +0530

    add set-04

---
+++ SET-04.py
@@ -0,0 +1,11 @@
+#1 Write a python program which covers all the methods (functions) of list. 
+
+list1=[1,2,3,4,5,67,234]
+'''print("list1:",list1)
+'''
+#methods
+list1.append(6)  #add last element in list
+print(list1)
+
+list1.insert(1,44) #add elelemet at specific index
+print(list1)
