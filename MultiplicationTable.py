17ae303e4d47f609aeb440f77cba3199f27f739d
Harsh Ghadiya <harshghadiya212@gmail.com>
2025-04-11 20:35:11 +0530

    Update MultiplicationTable.py

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -1,1 +1,8 @@
+# Multiplication table (from 1 to 10) in Python
+
+num = 12
+# To take input from the user
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
 
