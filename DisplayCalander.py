93d9ed5ee337c45e98e6afaee8d159929f4cf581
ghadiyaharsh <harshghadiya212@gmail.com>
2025-04-12 23:00:54 +0530

    calander program

--- DisplayCalander.py
+++ DisplayCalander.py
@@ -1,1 +1,17 @@
+#python program that display the calendar of the month and year
+
+# Import the calendar module
+import calendar
+
+# Get the month and year from the user
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+calendar.TextCalendar().prmonth(year, month) # Display the calendar for the given month and year
+
+
+
+
+
+
+
 
