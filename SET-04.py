d65ff5786738db30f4ff17cd76d7123d9411227a
ghadiyaharsh <harshghadiya212@gmail.com>
2025-04-30 11:33:25 +0530

    add program

--- SET-04.py
+++ SET-04.py
@@ -24,7 +24,6 @@ print(list1)     #[]
 list1.extend([76,89,34,234,87,567])
 print(list1)    #[76, 89, 34, 234, 87, 567]
 
-_______________________________________________________________________________________________________________________
 
 #2 Write a Python program to append a list to the second list.
 
@@ -35,7 +34,7 @@ print(list2)
 list2.extend(list1)
 print(list2)
 
-________________________________________________________________________________________________________________________
+
 
 
 #3 write a pyhton program to check the given list is palindrome or not.
@@ -45,13 +44,12 @@ if list1==list1[::-1]:
 else:
     print("list is not palindrome")
 
-________________________________________________________________________________________________________________________
     
 # 4 Write a python program to store strings in a list and then print them.
 list1=["hi","raj","jay","man"]
 print(list1)
 print(type(list1))
-__________________________________________________________________________________________________________________________
+
 
 #5 write a python program to print a list of palindrome number using loop by N numbers
 n=int(input("enter the number:"))
@@ -60,7 +58,7 @@ for i in range(1,n+1):
     if str(i)==str(i)[::-1]:
         list1.append(i)
         print(list1)
-__________________________________________________________________________________________________________________________        
+     
 
 #5 write a python program to print a list of prime numbers upto N using loop and clauses.
 def is_prime(num):
@@ -100,3 +98,17 @@ def print_even_numbers_up_to_n(N):
     return even_numbers
     
         
+#7 Write a python program to print the list of odd numbers using loop and clauses.
+
+def is_odd(num):
+    return num % 2 != 0
+def print_odd_numbers_up_to_n(N):
+    odd_numbers = []
+    
+    for num in range(1, N + 1):  # Loop from 1 to N
+        if is_odd(num):
+            odd_numbers.append(num)
+    return odd_numbers
+
+
+is_odd(12)
